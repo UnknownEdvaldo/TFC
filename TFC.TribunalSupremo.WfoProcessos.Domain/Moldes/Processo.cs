1ce449e425071910771ad1ac24b3997195cfd8de
Kcaxtanhu <6532820+Kcaxtanhu@users.noreply.github.com>
2018-08-01 21:29:25 +0100

    Endireitando o Domain

--- TFC.TribunalSupremo.WfoProcessos.Domain/Moldes/Processo.cs
+++ TFC.TribunalSupremo.WfoProcessos.Domain/Moldes/Processo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,7 +7,9 @@ namespace TFC.TribunalSupremo.WfoProcessos.Domain.Moldes
     public abstract class Processo
     {
         public Guid Id { get; set; }
+        public Guid ProvenienciaId { get; set; }
         public Proveniencia Proveniencia { get; set; }
+        public Guid Seccao { get; set; }
         public Seccao Seccao { get; set; }
     }
 }
