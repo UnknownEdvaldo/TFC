d9b00b9156fd2cd7c8bb38b4d9b4b424eb93634a
Kcaxtanhu <6532820+Kcaxtanhu@users.noreply.github.com>
2018-08-01 21:51:40 +0100

    Fechando o Ficheiro

--- TFC.TribunalSupremo.WfoProcessos.Domain/Moldes/ProcessoDistribuido.cs
+++ TFC.TribunalSupremo.WfoProcessos.Domain/Moldes/ProcessoDistribuido.cs
@@ -8,7 +8,7 @@ namespace TFC.TribunalSupremo.WfoProcessos.Domain.Moldes
     {
         public string Requerente { get; set; }
         public string Requerida { get; set; }
-        public int NumeroOrdem { get; set; }
+        public string NumeroOrdem { get; set; }
         public Especie EspecieRecurso { get; set; }
         public Guid RelatorId { get; set; }
         public Relator Relator { get; set; }
